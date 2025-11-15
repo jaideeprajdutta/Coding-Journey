eafe0a4c1163cf1d8938a8d0f01610b478b0a927
Jaideep Raj Dutta <sjaideeprajdutta@gmail.com>
2025-11-15 12:36:26 +0530

    removed exe files and updated .gitignore

---
+++ Day2/area_of_circle.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float radius = 4;
+    // float area = 3.14 * radius * radius;
+    // cout << area << endl;
+    cout<< 3.14 * radius * radius << endl;
+    
+}
